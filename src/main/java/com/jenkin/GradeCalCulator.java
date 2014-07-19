c22df92fe2c3572ab9ed4f9dd08d93a0ddc82aac
Baipor <amornjitranon@gmail.com>
2014-07-19 16:14:27 +0700

    Grade

--- src/main/java/com/jenkin/GradeCalCulator.java
+++ src/main/java/com/jenkin/GradeCalCulator.java
@@ -6,12 +6,29 @@ public class GradeCalCulator {
 		
 		String grade = "";
 		
-		if(score < 39){
-			
+		if(score <= 39){			
 			grade = "F";
+		}else if (score <= 49){
+			grade = "F+";
+		}else if (score <= 54){
+			grade = "D-";
+		}else if (score <= 59){
+			grade = "D";
+		}else if (score <= 64){
+			grade = "D+";
+		}else if (score <= 69){
+			grade = "C";
+		}else if (score <= 79){
+			grade = "B";
+		}else if (score <= 84){
+			grade = "A-";
+		}else if (score <= 94){
+			grade = "A";
+		}else if (score <= 100){
+			grade = "A+";
 		}
 		
-		return "";
+		return grade;
 	}
 
 }
