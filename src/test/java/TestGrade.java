40a84d7a1468ab00e8f441b5ebe5724f484c7c32
krittikorn <krittikorn@yahoo.com>
2014-07-19 16:13:06 +0700

    1

--- src/test/java/TestGrade.java
+++ src/test/java/TestGrade.java
@@ -10,17 +10,32 @@ import CalculatorSum.CalculatorSum;
 public class TestGrade {
 
 	@Test
-	public void testGrade_F() {		
+	public void testGrade_F0() {		
 		GradeCalCulator cal = new GradeCalCulator();
-		assertEquals("F", cal.showGrade(39));
+		assertEquals("F", cal.showGrade(0));
         // TODO: Remove this later
 	}
 	
 	@Test
-	public void testGrade_FPluss() {		
+	public void testGrade_F39() {		
 		GradeCalCulator cal = new GradeCalCulator();
 		assertEquals("F", cal.showGrade(39));
         // TODO: Remove this later
 	}
 
+	@Test
+	public void testGrade_FP40() {		
+		GradeCalCulator cal = new GradeCalCulator();
+		assertEquals("F", cal.showGrade(40));
+        // TODO: Remove this later
+	}
+	
+	@Test
+	public void testGrade_FP49() {		
+		GradeCalCulator cal = new GradeCalCulator();
+		assertEquals("F", cal.showGrade(49));
+        // TODO: Remove this later
+	}
+	
+	
 }
