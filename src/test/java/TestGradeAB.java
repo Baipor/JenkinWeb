a02a8c751883a2ad7b0bac30cb319f95047e3d2c
frozeny <frozeny@hotmail.com>
2014-07-19 16:21:44 +0700

    add Test Case A to B

---
+++ src/test/java/TestGradeAB.java
@@ -0,0 +1,56 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import com.jenkin.GradeCalCulator;
+
+public class TestGradeAB {
+	
+	GradeCalCulator obj = null;
+	
+	@Before
+    public void setUp() {
+		obj = new GradeCalCulator();
+    }
+	
+	@Test
+	public void test_Grade_A_Plus_100() {
+		assertEquals("A+", obj.showGrade(100));
+	}
+	
+	@Test
+	public void test_Grade_A_Plus_95() {
+		assertEquals("A+", obj.showGrade(95));
+	}
+	
+	@Test
+	public void test_Grade_A_94() {
+		assertEquals("A", obj.showGrade(94));
+	}
+	
+	@Test
+	public void test_Grade_A_85() {
+		assertEquals("A", obj.showGrade(85));
+	}
+	
+	@Test
+	public void test_Grade_A_Minus_84() {
+		assertEquals("A-", obj.showGrade(84));
+	}
+	
+	@Test
+	public void test_Grade_A_Minus_80() {
+		assertEquals("A-", obj.showGrade(80));
+	}
+	
+	@Test
+	public void test_Grade_B_79() {
+		assertEquals("B", obj.showGrade(79));
+	}
+	
+	@Test
+	public void test_Grade_B_70() {
+		assertEquals("B", obj.showGrade(70));
+	}
+
+}
