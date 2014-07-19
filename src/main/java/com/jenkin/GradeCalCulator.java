821426357f2602785df8145e5041006f70680e6b
Baipor <amornjitranon@gmail.com>
2014-07-19 15:55:26 +0700

    Grade

---
+++ src/main/java/com/jenkin/GradeCalCulator.java
@@ -0,0 +1,17 @@
+package com.jenkin;
+
+public class GradeCalCulator {
+	
+	public String showGrade(int score){
+		
+		String grade = "";
+		
+		if(score < 39){
+			
+			grade = "F";
+		}
+		
+		return "";
+	}
+
+}
---
+++ src/test/java/TestGrade.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.jenkin.GradeCalCulator;
+
+import CalculatorSum.CalculatorSum;
+
+
+public class TestGrade {
+
+	@Test
+	public void testGrade_F() {		
+		GradeCalCulator cal = new GradeCalCulator();
+		assertEquals("F", cal.showGrade(39));
+        // TODO: Remove this later
+	}
+	
+	@Test
+	public void testGrade_FPluss() {		
+		GradeCalCulator cal = new GradeCalCulator();
+		assertEquals("F", cal.showGrade(39));
+        // TODO: Remove this later
+	}
+
+}
