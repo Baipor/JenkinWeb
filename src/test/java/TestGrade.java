b913a272d3ad0bb24ce3cc217ca2f54d495b04ff
xlives <r.teeradaj@gmail.com>
2014-07-19 16:03:17 +0700

    * Modified test cases for calculating grades

--- src/test/java/TestGrade.java
+++ src/test/java/TestGrade.java
@@ -1,10 +1,6 @@
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-
 import com.jenkin.GradeCalCulator;
-
-import CalculatorSum.CalculatorSum;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 
 public class TestGrade {
@@ -19,8 +15,31 @@ public class TestGrade {
 	@Test
 	public void testGrade_FPluss() {		
 		GradeCalCulator cal = new GradeCalCulator();
-		assertEquals("F", cal.showGrade(39));
+		assertEquals("F+", cal.showGrade(49));
         // TODO: Remove this later
 	}
 
+    @Test
+    public void testGrade_C() {
+        GradeCalCulator cal = new GradeCalCulator();
+        assertEquals("C", cal.showGrade(69));
+    }
+
+    @Test
+    public void testGrade_D() {
+        GradeCalCulator cal = new GradeCalCulator();
+        assertEquals("D", cal.showGrade(59));
+    }
+
+    @Test
+    public void testGrade_DPlus() {
+        GradeCalCulator cal = new GradeCalCulator();
+        assertEquals("D+", cal.showGrade(64));
+    }
+
+    @Test
+    public void testGrade_DMinus() {
+        GradeCalCulator cal = new GradeCalCulator();
+        assertEquals("D-", cal.showGrade(54));
+    }
 }
