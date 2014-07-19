e5472af15f278fda9a50eb939f6ba932edbf59a9
Baipor <amornjitranon@gmail.com>
2014-07-19 16:33:06 +0700

    edit Div

--- src/test/java/com/TestDivide.java
+++ src/test/java/com/TestDivide.java
@@ -14,5 +14,13 @@ public class TestDivide {
 		assertEquals(2, calc.Divide(10,5));
         // TODO: Remove this later
 	}
+	
+	@Test
+	public void test0() {
+		Calc calc = new Calc();
+		assertEquals(0, calc.Divide(10,0));
+        // TODO: Remove this later
+	}
+
 
 }
